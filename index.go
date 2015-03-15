c35cb8f3fd2ac3c993845026f5ba1af9a001f495
Paul Swanson <paul@swanson.id.au>
2015-03-15 18:29:24 +1000

    Working torwards better error handling ...

--- index.go
+++ index.go
@@ -19,17 +19,15 @@ type replaceIndex []replaceItem
 //	index []replaceItem
 //}
 
+// TODO This func also needs to return an error - file read or csv error 
 func newReplaceIndex(r *csv.Reader) replaceIndex {
 	x := make(replaceIndex, 0)
 	for i := 0; ; i++ {
 		inputRow, err := r.Read()
-		if err != nil {
+		if inputRow == nil && err != nil {
+			// TODO return nil, err
 			break
 		}
-		// TODO Not sure how to handle EOF / non-EOF errors
-		//		if err != nil {
-		//			return i, err
-		//		}
 
 		fmt.Printf("Index item: %v, %v\n", []byte(inputRow[0]), []byte(inputRow[1]))
 		x = append(x, replaceItem{find: []byte(inputRow[0]), replace: []byte(inputRow[1])})
